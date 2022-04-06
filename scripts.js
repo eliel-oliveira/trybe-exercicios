fc7065486ef668dbeb2d4505faafe28026c6c6f6
Eliel Oliveira <elielsjc@hotmail.com>
2022-04-06 11:11:32 -0300

    Adiciona primeiro exercicio JavaScrit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const myName = "Eliel";
+const birthCity = "São José dos Campos";
+let birthYear = 1996;
+console.log(`Meu nome é ${myName}, nasci no ano de ${birthYear} na cidade de ${birthCity}`);
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
