d6282432676b81cabe76e9922908b0277cc345f8
Eliel Oliveira <elielsjc@hotmail.com>
2022-04-06 15:06:55 -0300

    adiciona exercicio 2 e 3 bloco 4.1

---
+++ scripts2.js
@@ -0,0 +1,9 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+    firstName: 'Ana',
+    lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+console.log(`a variavel patientId é ` + typeof patientId, 'a variavel isEnrolled é do tipo ' + typeof isEnrolled,'e a variavel email é do tipo ' + typeof patientEmail)
+console.log(typeof patientAge)
---
+++ scripts3.js
@@ -0,0 +1,5 @@
+const base = [5,8];
+const area = base[0]*base[1];
+const perimeter = base[0] + base [1] + base[0]+ base[1] ;
+console.log(area);
+console.log(perimeter)
