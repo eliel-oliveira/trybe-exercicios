b215a07a9f869deed55a420187247615fd5d1a9f
Eliel Oliveira <elielsjc@hotmail.com>
2022-04-07 16:16:38 -0300

    exercicios 3/9 feitos bloco 4.2

---
+++ exercicios.js
@@ -0,0 +1,13 @@
+console.log(`1 - Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;`)
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let index = 0;
+for (index = 0; index < numbers.length; index++) {
+    console.log(' numero ' + numbers[index])
+}
+console.log(`2 - Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;`)
+let soma = 0;
+for (index = 0; index < numbers.length; index++) {
+    console.log(soma += numbers[index])
+}
+console.log(`3 - Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;`)
+console.log (`A média aritimética é ${soma/index}`)
