680b96456e8dbd2bf9f3b8089477e3f71e071865
Eliel Oliveira <elielsjc@hotmail.com>
2022-05-02 19:07:53 -0300

    update test

---
+++ library.js
@@ -0,0 +1,12 @@
+// let moment = require("moment");
+
+// let brasilTetra = moment('17071994', 'DDMMYYYY');
+// let fromNow = brasilTetra.fromNow();
+
+// console.log(`o galvão boeno narrou o 'É TETRAA !!', há ${fromNow}`);
+
+
+cheet('↑ ↑ ↓ ↓', function () {
+    alert('WOW, que maravilha!');
+  });
+  
